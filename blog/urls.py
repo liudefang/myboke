e0e51dbd4538d20f20e13c87bee98fc05a67ab08
liudefang <15989576517@139.com>
2018-03-27 23:33:09 +0800

    博客系统

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls import url
 app_name='blog'
 urlpatterns = [
     url(r'^$',views.blog_title,name="blog_title"),
+    url(r'(?P<article_id>\d)/$',views.blog_article,name="blog_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ from blog.models import BlogArticles
 def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request,"blog/titles.html",{"blogs":blogs})
+
+def blog_article(request,article_id):   #①
+    article = BlogArticles.objects.get(id=article_id)  #②
+    pub = article.publish
+    return render(request,"blog/content.html",{"rticle":article,"publish":pub})
